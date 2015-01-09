b584372d6f1dfee5250657486c8e211e7b627a4d
Michael McKay <michael_mac_i@me.com>
2015-01-09 17:36:44 +0000

    Added in the 'rango' project
    Also learning how to map URLS.

---
+++ tango_with_django_project/rango/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, url
+from rango import views
+
+# This tuple must be called urlmappings for Django to pick up our URL
+# mappings.
+
+urlpatterns = patterns('',
+                       url(r'^$', views.index, name='index'))
