36b8ca616f92170d006a6fa29250bf460c143f5e
unknown <2093581M@BO706-03.ad.dcs.gla.ac.uk>
2015-01-12 13:56:06 +0000

    CHAPTER 005 - completed

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -4,8 +4,8 @@ from django.http import HttpResponse
 def index(request):
     # Construct a dictionary to pass to the template engine as
     # it's context. Note the key 'boldmessage' is the same as
-    # {{ boldmessage }} in the HTML template!
-
+    # {{ boldmessage }} in the HTML template.
+    
     context_dictionary = {'boldmessage' : "I am bold font from the context."}
     return render(request, 'rango/index.html', context_dictionary)
 
