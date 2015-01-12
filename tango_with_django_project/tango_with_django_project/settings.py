49da81359d801896ab3b37e37c2457def934208c
unknown <2093581M@BO706-03.ad.dcs.gla.ac.uk>
2015-01-12 14:37:06 +0000

    Added models for Django.

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -86,3 +86,11 @@ STATICFILES_DIRS = (
 # Media files.
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Databases.
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
