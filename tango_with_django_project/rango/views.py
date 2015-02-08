72008cd5103775f5d28a17512d745e66da8eb130
Michael McKay <michael_mac_i@me.com>
2015-02-08 23:23:49 +0000

    CHAPTER 010 - completed
    Done! Had some difficulty with one of the exercises "Change all the
    references to rango urls to use the url template tag.". Not fixed at
    this commit.

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -204,4 +204,4 @@ def user_logout(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html', {})
